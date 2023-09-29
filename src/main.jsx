f53f80a8f7cedca2605e55480b40b3b6db54e17e
Jamaoui Mouad <mouad_jamaoui@ext.carrefour.com>
2023-09-29 23:20:49 +0100

    5- TodoList with axios, react hook form

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import TodoList from "./TodoList.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <div className='container w-50 my-5'>
-            <TodoList/>
-        </div>
-    </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render()
